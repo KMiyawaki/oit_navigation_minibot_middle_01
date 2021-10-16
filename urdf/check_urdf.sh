525fb0f89684fb88d55e9790f303885586efc153
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2021-10-16 18:42:03 +0900

    fix: typo

--- urdf/check_urdf.sh
+++ urdf/check_urdf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rosrun xacro xacro --inorder minibot_light_01.xacro -o /tmp/robot.urdf
+rosrun xacro xacro --inorder minibot_middle_01.xacro -o /tmp/robot.urdf
 check_urdf /tmp/robot.urdf
